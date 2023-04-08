0403cf87280658deea6e0401b766076293f8eb1b
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2023-04-08 05:48:05 -0300

    feat: ✨ show country infos

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 
 
@@ -8,4 +10,8 @@ if __name__ == "__main__":
     country = input("Type the country name: ")
     response = requests.get(f"{BASE_URL}/name/{country}")
     if response.status_code == 200:
-        print(response.text)
+        infos = json.loads(response.text)[0]
+        print("##############################")
+        for data in infos:
+            print(f"{data}: {infos[data]}")
+        print("##############################")
